fb61f823dd207fde220709582edfca32a3750096
Deyan Sirakov <103333790+Deyan2306@users.noreply.github.com>
2025-10-13 15:32:21 +0300

    Create 02_sol.cpp

---
+++ week02 - variable types and bitwise operators/solutions/02_sol.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << " The sizeof(char) is: \t" << sizeof(char) << " bytes" << std::endl;
+    std::cout << " The sizeof(short) is: \t" << sizeof(short) << " bytes" << std::endl;
+    std::cout << " The sizeof(int) is: \t" << sizeof(int) << " bytes" << std::endl;
+    std::cout << " The sizeof(long) is: \t" << sizeof(long) << " bytes" << std::endl;
+    std::cout << " The sizeof(long long) is: \t" << sizeof(long long) << " bytes" << std::endl;
+    std::cout << " The sizeof(float) is: \t" << sizeof(float) << " bytes" << std::endl;
+    std::cout << " The sizeof(double) is: \t" << sizeof(double) << " bytes" << std::endl;
+    std::cout << " The sizeof(long double) is: \t" << sizeof(long double) << " bytes" << std::endl;
+    std::cout << " The sizeof(bool) is: \t" << sizeof(bool) << " bytes" << std::endl;
+
+	return 0;
+}
